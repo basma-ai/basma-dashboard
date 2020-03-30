51c6437c0c634d71b16bff2b90c5e965e7f0a688
Ali Ashoor <ali.ash.kh@gmail.com>
2020-03-30 23:28:14 +0300

    fix the link

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 // axios
 import axios from 'axios'
 
-// const baseURL = "https://video-api.basma.ai"
-const baseURL = "http://localhost:1061"
+const baseURL = "https://video-api.basma.ai"
+// const baseURL = "http://localhost:1061"
 // const baseURL = "https://ecda0df3.ngrok.io"
 
 export default axios.create({
